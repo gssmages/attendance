5ca67895674671f002e49e0f78a756dde829c68d
Magesh S <mageshbabu.s@flex.com>
2018-05-07 01:46:10 -0700

    [Webexplorer] Delete node "ispersonal", Connect action to page model "personalinfo", Delete connection from page model "personalinfo", Create node "ispersonalinfo", Update node "ispersonalinfo"

--- server/transform/attendance/personalinfo.js
+++ server/transform/attendance/personalinfo.js
@@ -1,3 +1,6 @@
 exports.personalinfo = async (session, models, vars) => {
     await session.transform.attendance.action('personalinfo', 'personalinfo');
 };
+exports.personalinfo2 = async (session, models, vars) => {
+    await session.transform.attendance.action('personalinfo', 'personalinfo');
+};
